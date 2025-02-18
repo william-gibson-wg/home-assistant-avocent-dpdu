d6ddd0bf5ab20a90ed1e61fbfef85198b9a95d4b
William Gibson <william.gibson.wg@gmail.com>
2025-02-18 01:34:12 +0000

    Find and replace template

--- custom_components/home-assistant-avocent-dpdu/__init__.py
+++ custom_components/home-assistant-avocent-dpdu/__init__.py
@@ -1,8 +1,8 @@
 """
-Custom integration to integrate integration_blueprint with Home Assistant.
+Custom integration to integrate home-assistant-avocent-dpdu with Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/william-gibson-wg/home-assistant-avocent-dpdu
 """
 
 from __future__ import annotations
--- custom_components/home-assistant-avocent-dpdu/data.py
+++ custom_components/home-assistant-avocent-dpdu/data.py
@@ -1,4 +1,4 @@
-"""Custom types for integration_blueprint."""
+"""Custom types for home-assistant-avocent-dpdu."""
 
 from __future__ import annotations
 
